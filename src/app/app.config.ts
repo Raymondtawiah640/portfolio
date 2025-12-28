520fda7ff73be472fb5c84a4cb2ea48dde33b8c3
Raymondtawiah640 <enterprisekiln@gmail.com>
2025-12-28 06:43:31 -0900

    MOdified it

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-=======
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
->>>>>>> c3704f358ee1d84d19d02f45d033e4f973b95e30
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -10,10 +6,7 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-<<<<<<< HEAD
-=======
     provideZoneChangeDetection({ eventCoalescing: true }),
->>>>>>> c3704f358ee1d84d19d02f45d033e4f973b95e30
     provideRouter(routes)
   ]
 };
