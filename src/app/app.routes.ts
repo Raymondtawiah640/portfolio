12e242ecfaf85e1775425862977866dd9a33ca25
Raymondtawiah640 <enterprisekiln@gmail.com>
2025-12-30 08:31:13 -0900

    Modified

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,3 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
-import { HomeComponent } from './pages/home/home.component';
-
-export const routes: Routes = [
-  { 
-    path: '', 
-    component: HomeComponent,
-    title: 'Raymond Kwame Tawiah - Software Engineer',
-    data: { animation: 'HomePage' }
-  },
-  { 
-    path: '**', 
-    redirectTo: '',
-    pathMatch: 'full'
-  }
-];
-=======
 
 export const routes: Routes = [];
->>>>>>> c3704f358ee1d84d19d02f45d033e4f973b95e30
